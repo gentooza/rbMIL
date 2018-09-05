4617a8ddfc2da728a0a5428405dc987075a3fd61
gentooza <joa.cuellar@riseup.net>
2018-09-05 12:22:19 +0200

    modified mask7.cpp and mask7_slots.h files to comply with Savannah policies

--- mask7.cpp
+++ mask7.cpp
@@ -1,3 +1,17 @@
+/*
+ * SCADA RB1000 - Plastic rolling machine RB1000, operator Interface
+ *
+ *  Copyright 2012-2018 by it's authors. 
+ *
+ *  Some rights reserved. See COPYING, AUTHORS.
+ *  This file may be used under the terms of the GNU General Public
+ *  License version 3.0 as published by the Free Software Foundation
+ *  and appearing in the file COPYING included in the packaging of
+ *  this file.
+ *
+ *  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ *  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 ////////////////////////////////////////////////////////////////////////////
 //
 // show_mask7 for ProcessViewServer created: mié jul 30 10:16:29 2014
