c6df210714a059a9c6ba1c41f8e62b1921193a9e
gentooza <joa.cuellar@riseup.net>
2018-09-11 10:11:17 +0200

    removed license script, it shouldn't be in repository

--- header_script.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
-
-for i in *.c *.h *.cpp #or *.h # or whatever other pattern...
-do
-  if ! grep -q Copyright $i
-  then
-    cat License_header_1011 $i >$i.new && mv $i.new $i
-  fi
-done
